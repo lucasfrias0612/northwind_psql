95f1ae5880cbf14f297567c0c51850fa14acc440
root <root@LucasUNPAZ.unpaz.edu.ar>
2019-10-08 16:53:58 -0300

    Se realizaró hasta el ejercicio 35 de la segunda parte (Selección avanzada, inner join, group by, having, where)

--- Ejercicios/Resoluciones/EjerciciosBasicosDeSeleccionDeRegistros/ej20.sql
+++ Ejercicios/Resoluciones/EjerciciosBasicosDeSeleccionDeRegistros/ej20.sql
@@ -1,1 +1,2 @@
---No entiendo la consigna
+select order_id as "ID de Orden", quantity as "Cantidad", unit_price as "Precio Unitario", round ((unit_price * quantity)::numeric, 2) AS "Monto Total"
+from order_details where unit_price * quantity between 10 and 300
--- Ejercicios/Resoluciones/EjerciciosBasicosDeSeleccionDeRegistros/ej22.sql
+++ Ejercicios/Resoluciones/EjerciciosBasicosDeSeleccionDeRegistros/ej22.sql
@@ -1,2 +1,2 @@
--- No encontré forma de duplicar repetidos en el último registro
+-- No encontré forma de duplicar repetidos en el último registro (o sea el equivalente a with ties en PostgreSQL)
 select * from products order by units_on_order desc limit 15
